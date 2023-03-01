492f676421ad503c298b01c19c7697b0bf8c57b8
Lucas Monteiro <lucmont00@gmail.com>
2023-03-01 05:36:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+   //numero inteiro = int
+   //numero casa decimal = float,double
+   //um caracter = char
+   //varios caracters = string 
+    int idade;
+    float altura ;
+    String nome;
+
+   //atribuição de valores
+    idade = 21;
+    altura = 1.73f;
+    nome = "Lucas";
+
+    
+   //mostrar valor
+   System.out.println("idade = "+idade);
+   System.out.println("altura = "+altura);
+   System.out.println(nome+" é o meu nome");
+    
+  }
+}
